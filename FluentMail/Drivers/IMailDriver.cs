e36d13fd80ee5ba93209003e89ccb9a9e919ec78
augustofrade <aquaaticnaotrava@gmail.com>
2025-06-14 21:38:54 -0300

    Add mail driver interface and driver options abstract class

---
+++ FluentMail/Drivers/IMailDriver.cs
@@ -0,0 +1,9 @@
+using FluentMail.Parts;
+
+namespace FluentMail.Drivers;
+
+public interface IMailDriver<in T> where T : MailDriverOptions
+{
+    Task Configure(T options);
+    Task<bool> SendAsync(MailScheme mailScheme);
+}
---
+++ FluentMail/Drivers/MailDriverOptions.cs
@@ -0,0 +1,3 @@
+namespace FluentMail.Drivers;
+
+public abstract class MailDriverOptions { }
